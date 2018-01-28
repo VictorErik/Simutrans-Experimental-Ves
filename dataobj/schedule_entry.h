44ea4f4759bc144bb33b21aa393310e973950ef6
James E. Petts <jamespetts@yahoo.com>
2018-01-28 14:30:35 +0000

    CODE: Initial data members for schedule_entry_t for all the new vehicle management features (features not yet implemented)

--- dataobj/schedule_entry.h
+++ dataobj/schedule_entry.h
@@ -27,8 +27,33 @@ public:
 	 */
 	koord3d pos;
 
+	enum schedule_entry_flag
+	{
+		wait_for_time_X					= (1u << 0),
+		lay_over						= (1u << 1),
+		ignore_choose					= (1u << 2),
+		force_range_stop				= (1u << 3),
+		conditional_depart_before_wait	= (1u << 4),
+		conditional_depart_after_wait	= (1u << 5),
+		conditional_skip				= (1u << 6),
+		send_trigger					= (1u << 7),
+		cond_trigger_is_line_or_cnv		= (1u << 8),
+		clear_stored_triggers_on_dep	= (1u << 9),
+		trigger_one_only				= (1u << 10),
+		couple							= (1u << 11),
+		uncouple						= (1u << 12),
+		couple_target_is_line_or_cnv	= (1u << 13),
+		uncouple_target_is_line_or_cnv	= (1u << 14),
+		uncouple_target_sch_is_reversed = (1u << 15)
+	};
+
+	/*
+	* A bitfield of flags of the type schedule_entry_flag (supra)
+	*/
+	uint16 flags;
+
 	/**
-	 * Wait for % load at this stops
+	 * Wait for % load at this stop
 	 * (ignored on waypoints)
 	 * @author Hj. Malthaner
 	 */
@@ -39,6 +64,56 @@ public:
 	 * @author Inkelyad
 	 */
 	sint16 spacing_shift;
+
+	/*
+	* A unique reference for this schedule entry that
+	* remains constant even when the sequence (and
+	* therefore the index) of this entry changes
+	*/
+	uint16 unique_entry_id;
+
+	/*
+	* A bitfield the conditions to trigger
+	* (in the range 1u << 0 to 1u << 15)
+	* when this convoy arrives at this stop
+	*/
+	uint16 condition_bitfield;
+
+	/*
+	* The ID of the line or convoy that will
+	* receive the trigger when a convoy with
+	* this schedule arrives at the stop
+	* with this entry.
+	*/
+	uint16 target_id_condition_trigger;
+
+	/*
+	* The ID of the line or convoy to which
+	* the convoy with this schedule will attempt
+	* to couple on reaching this destination.
+	*/
+	uint16 target_id_couple;
+
+	/*
+	* The ID of the line or convoy the schedule of
+	* which the convoy formed by the uncoupled vehicles
+	* from the convoy with this schedule will adopt on
+	* reaching the destination specified in this entry.
+	*/
+	uint16 target_id_uncouple;
+
+	/*
+	* The unique ID of the point in the schedule specified
+	* in target_id_couple from which the convoy comprising
+	* the vehicles uncoupled at this schedule entry will
+	* start. 
+	*
+	* There is no equivalent for couple because the 
+	* convoy to which this convoy will be coupled will 
+	* alerady be at the correct point in its schedule.
+	*/
+	uint16 target_unique_entry_uncouple;
+
 
 	/**
 	* maximum waiting time in 1/2^(16-n) parts of a month
@@ -57,9 +132,17 @@ public:
 	/**
 	 * Whether a convoy must wait for a 
 	 * time slot at this entry.
+	 * DEPRECATED ONCE THE FLAGS ARE PUT INTO SERVICE
 	 * @author: jamespetts
 	 */
 	bool wait_for_time;
+
+	bool is_flag_set(schedule_entry_flag flag) const { return flag & flags; }
+
+	void set_flag(schedule_entry_flag flag) { flags |= flag; }
+
+	void clear_flag(schedule_entry_flag flag) { flags &= ~flag; }
+
 };
 
 #endif
