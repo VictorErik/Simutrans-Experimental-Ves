a82a1c31ce7206bb93fc8b0739f62ecb8cbf4ab5
VictorErik <violinvictor@hotmail.com>
2019-01-24 15:35:33 +0100

    Fix: Removed old comments

--- gui/vehicle_manager_gui_list_info.h
+++ gui/vehicle_manager_gui_list_info.h
@@ -8,13 +8,10 @@
 
 class player_t;
 
+
+// "Upgrade" entries: Will list the difference between old and new vehicle
 #ifndef gui_upgrade_info_h
 #define gui_upgrade_info_h
-/**
-* One element of the vehicle list display
-*
-* @author Hj. Malthaner
-*/
 class gui_upgrade_info_t : public gui_world_component_t
 {
 private:
@@ -38,23 +35,15 @@ public:
 
 	vehicle_desc_t* get_upgrade() { return upgrade; }
 	vehicle_desc_t* get_existing() { return existing; }
-
-	/**
-	* Draw the component
-	* @author Hj. Malthaner
-	*/
 	void draw(scr_coord offset);
 
 };
 
 #endif
+
+// "Desc" entries: Will list basic info as well as specific info which is sorted by
 #ifndef gui_desc_info_h
 #define gui_desc_info_h
-/**
-* One element of the vehicle list display
-*
-* @author Hj. Malthaner
-*/
 class gui_desc_info_t : public gui_world_component_t
 {
 private:
@@ -62,20 +51,12 @@ private:
 	uint8 player_nr;
 	int sort_mode;
 	int display_mode;
-	/**
-	* Handle Convois to be displayed.
-	* @author Hj. Malthaner
-	*/
 	vehicle_desc_t* veh;
 	int image_height;
 
 	uint16 amount;
 
 public:
-	/**
-	* @param cnv, the handler for the Convoi to be displayed.
-	* @author Hj. Malthaner
-	*/
 	gui_desc_info_t(vehicle_desc_t* veh, uint16 amount, int sortmode_index, int displaymode_index);
 
 	bool infowin_event(event_t const*) OVERRIDE;
@@ -85,30 +66,18 @@ public:
 	bool set_selection(bool sel) { return selected = sel; }
 	vehicle_desc_t* get_desc() { return veh; }
 	int get_image_height() { return image_height; }
-
-	/**
-	* Draw the component
-	* @author Hj. Malthaner
-	*/
 	void draw(scr_coord offset);
 
 };
 #endif
+
+// "Veh" entries: Will list basic info as well as specific info which is sorted by
 #ifndef gui_veh_info_h
 #define gui_veh_info_h
-/**
-* One element of the vehicle list display
-*
-* @author Hj. Malthaner
-*/
 class gui_veh_info_t : public gui_world_component_t
 {
 private:
 	player_t *player;
-	/**
-	* Handle Convois to be displayed.
-	* @author Hj. Malthaner
-	*/
 	vehicle_t* veh;
 
 	sint32 mean_convoi_speed;
@@ -117,18 +86,9 @@ private:
 	int image_height;
 
 public:
-	/**
-	* @param cnv, the handler for the Convoi to be displayed.
-	* @author Hj. Malthaner
-	*/
 	gui_veh_info_t(vehicle_t* veh);
 
 	bool infowin_event(event_t const*) OVERRIDE;
-
-	/**
-	* Draw the component
-	* @author Hj. Malthaner
-	*/
 	void draw(scr_coord offset);
 
 	bool is_selected() { return selected; }
